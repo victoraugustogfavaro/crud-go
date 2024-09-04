a4f0fa37f9894bda7a70d7244c1773a1cbbcb0b3
victoraugustogfavaro <victoraugustogfavaro@gmail.com>
2024-09-04 18:50:08 -0300

    Validações

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -2,8 +2,8 @@ package request
 
 // criando objetos
 type UserRequest struct {
-	Email    string `json: "email"`
-	Password string `json: "password"`
-	Name     string `json: "name"`
-	Age      int8   `json: "age"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6,containsAny=!@#$%*"`
+	Name     string `json:"name" binding:"required,min=4,max=50"`
+	Age      int8   `json:"age" binding:"required,numeric,min=1,max=40"`
 }
